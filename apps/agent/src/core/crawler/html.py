fcb283a99fa205cbf506f317110cebddf5640ed0
2uanDM <hokage321xxx@gmail.com>
2024-12-24 23:56:26 +0700

    remove unescessary stuffs

--- apps/agent/src/core/crawler/html.py
+++ apps/agent/src/core/crawler/html.py
@@ -1,8 +1,14 @@
+import asyncio
 import json
-import httpx
+from typing import Dict, List
 
+import httpx
 from playwright.async_api import TimeoutError, async_playwright
+from rich.console import Console
+
 from .base import BaseCrawler
+
+console = Console()
 
 
 class HtmlCrawler(BaseCrawler):
@@ -11,15 +17,14 @@ class HtmlCrawler(BaseCrawler):
 
         self.headers = self.config.get("headers", self.base_headers)
         self.headless = self.config.get("headless", True)
-        self.timeout = self.config.get("timeout", 5_000)
-
-        # Before the browser closes, take a screenshot or not
+        self.timeout = self.config.get("timeout", 10000)
+        self.cache_ttl = self.config.get("cache_ttl", 600)
         self.end_with_capture = self.config.get("end_with_capture", False)
-
-        # Fetch strategy: requests or browser or hybrid (default: hybrid)
         self.fetch_strategy = self.config.get("fetch_strategy", "hybrid")
+        self.max_recursive_urls = self.config.get("max_recursive_urls", 6)
 
-    async def fetch_by_requests(self, url: str) -> str:
+    async def _fetch_by_requests(self, url: str) -> str:
+        print(f"==>> url: {url}")
         try:
             async with httpx.AsyncClient(
                 timeout=5,
@@ -37,7 +42,8 @@ class HtmlCrawler(BaseCrawler):
                 "urls": [],
             }
 
-    async def fetch_by_browser(self, url: str) -> str:
+    async def _fetch_by_browser(self, url: str) -> str:
+        print(f"==>> url: {url}")
         async with async_playwright() as p:
             browser = await p.chromium.launch(
                 headless=self.headless,
@@ -66,6 +72,23 @@ class HtmlCrawler(BaseCrawler):
                     await page.screenshot(path="screenshot.png")
                 await browser.close()
 
+    async def fetch(self, url: str) -> List[Dict[str, str]]:
+        if self.fetch_strategy == "requests":
+            data = await self._fetch_by_requests(url)
+        elif self.fetch_strategy == "browser":
+            data = await self._fetch_by_browser(url)
+        elif self.fetch_strategy == "hybrid":
+            try:
+                data = await self._fetch_by_requests(url)
+                console.print("[bold cyan]Fetched by requests[/bold cyan]")
+            except httpx.HTTPStatusError:
+                data = await self._fetch_by_browser(url)
+                console.print("[bold magenta]Fetched by browser[/bold magenta]")
+        else:
+            raise ValueError(f"Invalid fetch strategy: {self.fetch_strategy}")
+
+        return data
+
     async def run(self, url: str) -> str:
         # Check cache first
         cached_data = self.cache_db.get(url)
@@ -74,21 +97,22 @@ class HtmlCrawler(BaseCrawler):
             print("CACHE HIT: {}".format(url))
             return json.loads(cached_data.decode("utf-8"))
 
-        if self.fetch_strategy == "requests":
-            data = await self.fetch_by_requests(url)
-        elif self.fetch_strategy == "browser":
-            data = await self.fetch_by_browser(url)
-        elif self.fetch_strategy == "hybrid":
-            try:
-                data = await self.fetch_by_requests(url)
-                print("Fetched by requests")
-            except httpx.HTTPStatusError:
-                data = await self.fetch_by_browser(url)
-                print("Fetched by browser")
-        else:
-            raise ValueError(
-                "Invalid fetch strategy: {}".format(self.fetch_strategy)
-            )
+        # Firsts fetch
+        data = await self.fetch(url)
+
+        urls = data.get("urls", [])
+
+        tasks = [self.fetch(url) for url in urls[: self.max_recursive_urls]]
+
+        first_depth_data = await asyncio.gather(*tasks)
+
+        total_markdown = (
+            data["markdown"]
+            + "\n\n"
+            + "\n\n".join([d["markdown"] for d in first_depth_data])
+        )
+
+        data["markdown"] = total_markdown
 
         # Cache the data
         self.cache_db.set(
